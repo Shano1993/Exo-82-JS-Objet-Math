d8f8ee8553019967987026db8a7be1fa2401a565
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-25 15:17:06 +0100

    math

---
+++ exo.js
@@ -0,0 +1,131 @@
+/*
+    1
+ */
+
+let numberOne = 34.589456;
+numberOne = Math.floor(numberOne);
+console.log(numberOne);
+
+/*
+    2
+ */
+
+let numberTwo = 68.412784;
+numberTwo = Math.floor(numberTwo);
+console.log(numberTwo);
+
+/*
+    3
+ */
+
+let numberThree = 39.456;
+numberThree = Math.ceil(numberThree);
+console.log(numberThree);
+
+/*
+    4
+ */
+
+let numberFour = 27.34876541;
+numberFour = Math.ceil(numberFour);
+console.log(numberFour);
+
+/*
+    5
+ */
+
+let numberFive = 5.68;
+numberFive = Math.round(numberFive);
+console.log(numberFive);
+
+/*
+    6
+ */
+
+let numberSix = 5.41;
+numberSix = Math.round(numberSix);
+console.log(numberSix);
+
+/*
+    7
+ */
+
+let numberSeven = 6.12457;
+numberSeven = Math.trunc(numberSeven);
+console.log(numberSeven);
+
+/*
+    8
+ */
+
+let numberEight = 9.568124;
+numberEight = Math.trunc(numberEight);
+console.log(numberEight);
+
+/*
+    9
+ */
+
+let a1 = 5;
+let b1 = 1;
+let c1 = 12;
+let d1 = -1;
+let e1 = 45;
+
+let minimum1 = Math.min(a1,b1,c1,d1,e1);
+console.log(minimum1);
+
+/*
+    10
+ */
+
+let a2 = 9;
+let b2 = 12;
+let c2 = 16;
+let d2 = 44;
+let e2 = 3;
+
+let minimum2 = Math.min(a2,b2,c2,d2,e2);
+console.log(minimum2);
+
+/*
+    11
+ */
+
+let a3 = 5;
+let b3 = 1;
+let c3 = 12;
+let d3 = -1;
+let e3 = 45;
+
+let maximum1 = Math.max(a3,b3,c3,d3,e3);
+console.log(maximum1);
+
+/*
+    12
+ */
+
+let a4 = 9;
+let b4 = 12;
+let c4 = 16;
+let d4 = 44;
+let e4 = 3;
+
+let maximum2 = Math.max(a4,b4,c4,d4,e4);
+console.log(maximum2);
+
+/*
+    13
+ */
+
+let abs1 = -6;
+abs1 = Math.abs(abs1);
+console.log(abs1);
+
+/*
+    14
+ */
+
+let abs2 = -56;
+abs2 = Math.abs(abs2);
+console.log(abs2);
